49b77a27d176e67dee4386af35ce562251cb0d2a
Chmuradin <62878697+Chmuradin@users.noreply.github.com>
2023-05-08 09:29:09 +0200

    Beginning of histogram

--- src/routes/details/[cur_id]/Controls.svelte
+++ src/routes/details/[cur_id]/Controls.svelte
@@ -1,13 +1,13 @@
 <script>
     // Properties
-    export let datar;
+    export let data;
     export const ssr = false;
     export let onChange;
 
 </script>
     
 <div>
-  <input type="range" class="form-range" min="0" max="20160" bind:value={datar.minuteSlider} on:change={onChange} id="slider" style="margin: 0; width: 300px;">
+  <input type="range" class="form-range" min="0" max="20160" bind:value={data.minuteSlider} on:change={onChange} id="slider" style="margin: 0; width: 300px;">
 </div> 
     
   
