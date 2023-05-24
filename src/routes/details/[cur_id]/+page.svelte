85793888502b4c607ca18c2cbae727ae1371cc7f
Chmuradin <62878697+Chmuradin@users.noreply.github.com>
2023-05-24 18:14:55 +0200

    Fixing stuffff

--- src/routes/details/[cur_id]/+page.svelte
+++ src/routes/details/[cur_id]/+page.svelte
@@ -1,18 +1,63 @@
 <script>
+	// Dependencies
 	import {goto} from '$app/navigation';
 	import Scatter from './Scatter.svelte'
 	import Slider from './Slider.svelte'
 	import Barchart from './Barchart.svelte'
 	export let data;
+	// Iterator - written because I wanted to skip unwanted ids
+	const u_ids = [...new Set(data.GPSTracking.map((v) => v.car_id))];
+	let cursor = u_ids.findIndex((id) => id === data.currentCarId);
+
+	function iter(cursor, array) {
+		function next() {
+			const nextIndex = cursor + 1;
+			if(isIndexInbound(nextIndex, array)) {
+				return {
+					value: array[nextIndex],
+					nextCursor: nextIndex,
+				}
+			}
+			return {nextCursor: cursor, value: array[cursor]};
+		}
+		function previous() {
+			const nextIndex = cursor - 1;
+			if(isIndexInbound(nextIndex, array)) {
+				return {
+					value: array[nextIndex],
+					nextCursor: nextIndex,
+				}
+			}
+
+			return {nextCursor: cursor, value: array[cursor]};
+		}
+
+		function isIndexInbound(index, array) {
+			return index <= array.length - 1 && index >= 0;
+		}
+
+		return { next, previous };
+	} 
+
 	function previousPage() {
-		goto(`/details/${data.currentCarId - 1}`);
+		const { value, nextCursor } = iter(cursor, u_ids).previous();
+
+		console.log(value, nextCursor);
+
+		cursor = nextCursor;
+		
+		goto(`/details/${value}`);
 	}
 
 	function nextPage() {
-		goto(`/details/${data.currentCarId + 1}`);
+		const { value, nextCursor } = iter(cursor, u_ids).next();
+		cursor = nextCursor;
+		
+		console.log(value, nextCursor);
+
+		goto(`/details/${value}`);
 	}
 </script>
-
 
 <div>
 	<a href="/">Car overview</a>
